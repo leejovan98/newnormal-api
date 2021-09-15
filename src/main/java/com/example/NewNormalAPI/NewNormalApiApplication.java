57eb9ac393b441219945bc495933836c4e921ce8
Jovan <leejovan98_dv@outlook.sg>
2021-09-15 15:25:58 +0800

    First Commit

---
+++ src/main/java/com/example/NewNormalAPI/NewNormalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewNormalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewNormalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewNormalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewNormalAPI/NewNormalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewNormalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewNormalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
