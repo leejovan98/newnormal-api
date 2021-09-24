b0a9f260a4e79b6846f1381ff7d406c56b9397c4
ChongJun123 <cjchew.2020@scis.smu.edu.sg>
2021-09-24 14:38:58 +0800

    Added method information

--- src/main/java/com/example/NewNormalAPI/user/UserController.java
+++ src/main/java/com/example/NewNormalAPI/user/UserController.java
@@ -14,11 +14,16 @@ import javax.validation.Valid;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import jdk.jfr.Description;
+
 @RestController
 public class UserController {
+    @Autowired
     private UserRepository users;
+    @Autowired
     private BCryptPasswordEncoder encoder;
 
+    // Constructor
     public UserController(UserRepository users, BCryptPasswordEncoder encoder) {
         this.users = users;
         this.encoder = encoder;
@@ -30,13 +35,17 @@ public class UserController {
     // }
 
     /**
-    * Using BCrypt encoder to encrypt the password for storage 
+    * Checks if User already exists in the database
     * @param user
-    * @return 
+    * @throws userAlreadyExistsException TODO: Create exception
+    * @return user
     */
     @PostMapping("/users")
-    public User addUser(@Valid @RequestBody User user){
-        // use service to check if user is already in database
+    public User addUser(@Valid @RequestBody User user) throws UserAlreadyExistsException{
+        //TODO: use service to check if user is already in database
+        if (/* user already in database*/) {
+            // throw exception
+        }
         user.setPassword(encoder.encode(user.getPassword()));
         return users.save(user);
     }
