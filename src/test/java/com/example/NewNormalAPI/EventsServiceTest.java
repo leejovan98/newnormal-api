18587be7bc1f15ccddb32b19f5a131880b33dde3
ChongJun123 <cjchew.2020@scis.smu.edu.sg>
2021-10-13 13:42:41 +0800

    minor changes

--- src/test/java/com/example/NewNormalAPI/EventsServiceTest.java
+++ src/test/java/com/example/NewNormalAPI/EventsServiceTest.java
@@ -1,8 +1,6 @@
 package com.example.NewNormalAPI;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
