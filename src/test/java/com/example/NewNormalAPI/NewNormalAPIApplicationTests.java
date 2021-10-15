bb4db1f2754c6ab35344365dc016e970fa3f7862
ChongJun123 <cjchew.2020@scis.smu.edu.sg>
2021-10-15 17:23:22 +0800

    Update NewNormalAPIApplicationTests.java

--- src/test/java/com/example/NewNormalAPI/NewNormalAPIApplicationTests.java
+++ src/test/java/com/example/NewNormalAPI/NewNormalAPIApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.NewNormalAPI;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.NewNormalAPI.event.EventRepository;
+import com.example.NewNormalAPI.user.UserRepository;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NewNormalAPIApplicationTests {
+	
+	@Autowired
+	private EventRepository events;
 
+	@Autowired
+	private UserRepository users;
+
+	@AfterEach
+	void tearDown() {
+		// clear the database after each test
+		events.deleteAll();
+		users.deleteAll();
+	}
+	
 	@Test
 	void contextLoads() {
+		assertEquals(123, 123);
 	}
 
 }
