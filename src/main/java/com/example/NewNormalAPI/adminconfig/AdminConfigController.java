b1b24f4053a539ffad7b2cf75887bea0bb647aaf
Jovan <leejovan98_dv@outlook.sg>
2021-11-04 21:45:04 +0800

    Changes for admin config

--- src/main/java/com/example/NewNormalAPI/adminconfig/AdminConfigController.java
+++ src/main/java/com/example/NewNormalAPI/adminconfig/AdminConfigController.java
@@ -1,23 +1,21 @@
 package com.example.NewNormalAPI.adminconfig;
 
 import java.util.List;
+import java.util.Objects;
 
-import com.example.NewNormalAPI.event.Event;
-import com.example.NewNormalAPI.event.EventsService;
-import com.example.NewNormalAPI.user.User;
-import com.example.NewNormalAPI.user.UserDetailsServiceImpl;
-import com.example.NewNormalAPI.venue.VenueRepo;
-import com.example.NewNormalAPI.venue.VenueService;
-
-import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.NewNormalAPI.event.EventsService;
+import com.example.NewNormalAPI.user.User;
+import com.example.NewNormalAPI.user.UserDetailsServiceImpl;
 
 @RestController
 public class AdminConfigController {
@@ -42,15 +40,39 @@ public class AdminConfigController {
         user.setAuthorities("faculty");
         userSvc.update(user);
     }
+    
+    @GetMapping("/admin/configuration")
+    @ResponseStatus(HttpStatus.OK)
+    public List<AdminConfig> getConfigs() {
+    	return adminConfigSvc.getAllAdminConfig();
+    }
 
     // Update capacity
     @PostMapping("/admin/configuration")
     @ResponseStatus(HttpStatus.OK)
-    public void updateAdminConfig(AdminConfig adminConfig) throws PropertyDoesNotExistException {
+    public void updateAdminConfig(@RequestBody AdminConfig adminConfig) throws PropertyDoesNotExistException {
         try {
             adminConfigSvc.update(adminConfig);
         } catch (PropertyDoesNotExistException e) {
-            System.out.println(e.getMessage());
+        	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Property Value Received");
         }
+    }
+    
+    @GetMapping("/admin/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+    	return userSvc.loadAllUsers();
+    }
+    
+    @PostMapping("/admin/users")
+    @ResponseStatus(HttpStatus.OK)
+    public void promoteUser(@RequestBody User user) {
+    	User curUser = userSvc.loadUserEntityByUsername(user.getUsername());
+    	if(Objects.isNull(curUser)) {
+    		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Such User");
+    	}
+    	curUser.setAuthorities("faculty");
+    	userSvc.update(curUser);
+    	
     }
 }
