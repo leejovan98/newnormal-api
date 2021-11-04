b795950bdc091a1620a5d22256659d345b564465
ChongJun123 <cjchew.2020@scis.smu.edu.sg>
2021-11-04 18:27:28 +0800

    venue many2one

--- src/main/java/com/example/NewNormalAPI/venue/VenueTypeInfo.java
+++ src/main/java/com/example/NewNormalAPI/venue/VenueTypeInfo.java
@@ -1,8 +1,10 @@
 package com.example.NewNormalAPI.venue;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -11,6 +13,7 @@ import lombok.Data;
 public class VenueTypeInfo {
     @Id
     @GeneratedValue
+    @OneToMany(mappedBy = "type", orphanRemoval = true, cascade = CascadeType.ALL)
     private String venue_name;
     
     private int capacity;
