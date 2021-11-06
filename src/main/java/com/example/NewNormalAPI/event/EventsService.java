29a7a7e5d42d2c8644f5183a521df06fb92b5243
Ashley Loh <ashley.loh.2020@scis.smu.edu.sg>
2021-11-06 23:29:26 +0800

    EventsService.java javadocs

--- src/main/java/com/example/NewNormalAPI/event/EventsService.java
+++ src/main/java/com/example/NewNormalAPI/event/EventsService.java
@@ -18,11 +18,24 @@ public class EventsService {
         this.eRepo = eRepo;
     }
 
+    /**
+     * Determines whether location is already in use or not
+     * 
+     * @param event
+     * @return true if 
+     */
     public boolean locationAlreadyInUse(Event event) {
         List<Event> myList = eRepo.findByLocationAndStartDatetime(event.getLocation(), event.getStartDatetime());
         return !myList.isEmpty();
     }
 
+    /**
+     * Saves an event
+     * 
+     * @param event
+     * @return the saved event
+     * @throws LocationAlreadyInUseException
+     */
     public Event save(Event event) throws LocationAlreadyInUseException {
     	if(locationAlreadyInUse(event)) {
     		throw new LocationAlreadyInUseException("Location has already been booked for this timeslot.");
@@ -30,20 +43,42 @@ public class EventsService {
         return eRepo.save(event);
     }
     
+    /**
+     * Updates the event
+     * 
+     * @param event
+     * @return the updated event
+     */
     public Event update(Event event) {
         return eRepo.save(event);
     }
     
+    /**
+     * Allows uses to get the event by using invite code
+     * 
+     * @param inviteCode
+     * @return event under respective invite code
+     */
     public Event getEventByInviteCode(String inviteCode) {
     	Optional<Event> opt = eRepo.findByInviteCode(inviteCode);
     	if(opt.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid invitation code");
     	return opt.get();
     }
     
+    /**
+     * Returns list of featured public events
+     * 
+     * @return list of featured public events
+     */
     public List<Event> getFeaturedPublicEvents(){
     	return eRepo.findTop10ByVisibilityOrderByStartDatetimeAsc("public");
     }
 
+    /**
+     * Returns list of all events
+     * 
+     * @return list of all events
+     */
     public List<Event> getAllEvents() {
         return eRepo.findAll();
     }
