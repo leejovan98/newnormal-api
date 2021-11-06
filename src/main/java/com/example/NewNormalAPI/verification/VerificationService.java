307f0706fb2e717d472d28bb7104382743d41081
Ashley Loh <ashley.loh.2020@scis.smu.edu.sg>
2021-11-06 23:54:11 +0800

    VerificationService.java javadocs

--- src/main/java/com/example/NewNormalAPI/verification/VerificationService.java
+++ src/main/java/com/example/NewNormalAPI/verification/VerificationService.java
@@ -11,14 +11,31 @@ public class VerificationService{
 	@Autowired
 	private VerificationRepository verifications;
 	
+	/**
+	 * Deletes verification of account creation
+	 * 
+	 * @param verification
+	 */
 	public void delete(Verification verification) {
 		verifications.delete(verification);
 	}
 	
+	/**
+	 * Saves verification of account creation
+	 * 
+	 * @param verification
+	 * @return
+	 */
 	public Verification save(Verification verification) {
 		return verifications.save(verification);
 	}
 	
+	/**
+	 * Finds verification code in repository
+	 * 
+	 * @param verificationCode
+	 * @return verification
+	 */
 	public Optional<Verification> findById(String verificationCode){
 		return verifications.findById(verificationCode);
 	}
