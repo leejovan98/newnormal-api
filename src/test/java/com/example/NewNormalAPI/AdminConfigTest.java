afaf6fc5c9e9e100dbf5088d83b1240a852326f6
Wesley Au <wesley.au.2020@scis.smu.edu.sg>
2021-11-08 16:31:49 +0800

    Updated AdminConfigTest.java - 3

--- src/test/java/com/example/NewNormalAPI/AdminConfigTest.java
+++ src/test/java/com/example/NewNormalAPI/AdminConfigTest.java
@@ -46,15 +46,20 @@ public class AdminConfigTest {
 
     @Test
     void updateAdminConfig_newValue_ReturnSavedAdminConfig() {
+        // arrange
         AdminConfig adminConfig = new AdminConfig();
         adminConfig.setProperty("CAPACITY");
         adminConfig.setValue("100");
 
-        // mock the "save" operation
+        // mock the "save" operation - stubbing
         when(adminConfigs.save(any(AdminConfig.class))).thenReturn(adminConfig);
 
-        // verify
+        // act
+        AdminConfig testResult = adminConfigService.save(adminConfig);
+
+        // assert
         verify(adminConfigs).save(adminConfig);
+        assertEquals(adminConfig, testResult);
     }
 
     @Test
