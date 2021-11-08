63f62f11c5fce938a852350f04eb83979ec6090c
Wesley Au <wesley.au.2020@scis.smu.edu.sg>
2021-11-08 17:43:42 +0800

    Updated AdminConfigTest.java - 5
    I realized that saveEvent_AllowAdjacentBookingsFalse_ThrowAdjacentBookingException() should be under integration testing because it needs more than one service.

--- src/test/java/com/example/NewNormalAPI/AdminConfigTest.java
+++ src/test/java/com/example/NewNormalAPI/AdminConfigTest.java
@@ -1,7 +1,6 @@
 package com.example.NewNormalAPI;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -9,20 +8,13 @@ import static org.mockito.Mockito.when;
 import com.example.NewNormalAPI.adminconfig.AdminConfig;
 import com.example.NewNormalAPI.adminconfig.AdminConfigRepo;
 import com.example.NewNormalAPI.adminconfig.AdminConfigService;
-import com.example.NewNormalAPI.event.Event;
-import com.example.NewNormalAPI.event.EventRepository;
-import com.example.NewNormalAPI.event.EventsService;
-import com.example.NewNormalAPI.venue.Venue;
-import com.example.NewNormalAPI.venue.VenueTypeInfo;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 @ExtendWith(MockitoExtension.class)
 public class AdminConfigTest {
@@ -32,12 +24,6 @@ public class AdminConfigTest {
 
     @InjectMocks
     private AdminConfigService adminConfigService;
-
-    @Mock
-    private EventRepository events;
-
-    @InjectMocks
-    private EventsService eventsService;
 
     @AfterEach
     void tearDown() {
@@ -61,36 +47,5 @@ public class AdminConfigTest {
         // assert
         verify(adminConfigs).save(adminConfig);
         assertEquals(adminConfig, testResult);
-    }
-
-    @Test
-    void saveEvent_AllowAdjacentBookingsFalse_ThrowAdjacentBookingException() {
-        // arrange
-        AdminConfig adminConfig = new AdminConfig();
-        adminConfig.setProperty("ALLOW_ADJACENT_BOOKINGS");
-        adminConfig.setValue("N");
-
-        VenueTypeInfo newVenueTypeInfo = new VenueTypeInfo();
-        newVenueTypeInfo.setVenueType("SR");
-
-        Venue newVenue = new Venue();
-        newVenue.setBuilding("SCIS");
-        newVenue.setVenueTypeInfo(newVenueTypeInfo);
-        newVenue.setLevel(2);
-        newVenue.setRoomNumber(2);
-
-        Event newEvent = new Event();
-        newEvent.setVenue(newVenue);
-
-        // stubbing
-        when(events.save(any(Event.class))).thenReturn(newEvent);
-
-        // act
-        ResponseStatusException thrown = assertThrows(ResponseStatusException.class,
-                () -> eventsService.save(newEvent));
-
-        // assert
-        verify(events).save(newEvent);
-        assertEquals(HttpStatus.FORBIDDEN, thrown.getStatus());
     }
 }
