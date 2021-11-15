65700c8e43a48b6ece2785e5abeecf14ce0e71f5
Ashley Loh <ashley.loh.2020@scis.smu.edu.sg>
2021-11-15 20:50:12 +0800

    clean up code in LocationAlreadyInUseException.java

--- src/main/java/com/example/NewNormalAPI/event/LocationAlreadyInUseException.java
+++ src/main/java/com/example/NewNormalAPI/event/LocationAlreadyInUseException.java
@@ -1,9 +1,6 @@
 package com.example.NewNormalAPI.event;
 
 public class LocationAlreadyInUseException extends RuntimeException {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public LocationAlreadyInUseException(String message) {
