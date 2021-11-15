8b869616a16451fb32dbfc52b20e0bdf2a1de65a
Ashley Loh <ashley.loh.2020@scis.smu.edu.sg>
2021-11-15 20:49:04 +0800

    clean up code

--- src/main/java/com/example/NewNormalAPI/event/UserNotAuthorisedException.java
+++ src/main/java/com/example/NewNormalAPI/event/UserNotAuthorisedException.java
@@ -1,9 +1,6 @@
 package com.example.NewNormalAPI.event;
 
 public class UserNotAuthorisedException extends Exception {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public UserNotAuthorisedException() {
